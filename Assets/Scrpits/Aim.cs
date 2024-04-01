49869ec2db5dae89c40d04b9aabfda1f28810bbf
MarcusReyes619 <129239355+MarcusReyes619@users.noreply.github.com>
2024-04-01 11:50:08 -0600

    4/1 Caster and OverLap scrip and Colider scene

--- Assets/Scrpits/Aim.cs
+++ Assets/Scrpits/Aim.cs
@@ -5,35 +5,30 @@ using UnityEngine;
 public class Aim : MonoBehaviour
 {
     [SerializeField] float speed = 3;
-    Vector3 rotation = Vector3.zero;
-    Vector2 preAxis = Vector2.zero;
 
-    // Start is called before the first frame update
+    Vector3 rotation = Vector3.zero;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        preAxis.x = -Input.GetAxis("Mouse Y");
-        preAxis.y = Input.GetAxis("Mouse X");
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 axis = Vector3.zero;
-        axis.x = -Input.GetAxis("Mouse Y") - preAxis.x;
-        axis.y = Input.GetAxis("Mouse X") - preAxis.y;
+        axis.x = -Input.GetAxis("Mouse Y");
+        axis.y = Input.GetAxis("Mouse X");
 
         rotation.x += axis.x * speed;
         rotation.y += axis.y * speed;
 
         rotation.x = Mathf.Clamp(rotation.x, -50, 50);
-        rotation.y = Mathf.Clamp(rotation.y, -50, 50);
+        rotation.y = Mathf.Clamp(rotation.y, -70, 70);
 
         Quaternion qyaw = Quaternion.AngleAxis(rotation.y, Vector3.up);
         Quaternion qpitch = Quaternion.AngleAxis(rotation.x, Vector3.right);
 
-        transform.localRotation = (qyaw * qpitch);
+        transform.localRotation = qyaw * qpitch;
     }
 }
